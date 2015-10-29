001f71e76333cd07b0659d8de6fa0e8f170ebddc
Paul Massey <paul.massey@tenforce.com>
2015-10-29 11:14:30 +0100

    final updates for machine

--- install_dcatap_validator.sh
+++ install_dcatap_validator.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 ###################################################################################
+# Install script assuming everything in the current directory.
+#
+WORKDIR=`pwd`
+
+###################################################################################
 # Development tools should have been installed.
 curl --silent --location https://rpm.nodesource.com/setup | bash -
 yum -y install nodejs
@@ -12,14 +17,14 @@ pushd /var/local/dcat-ap_validator
  wget -N http://archive.apache.org/dist/jena/binaries/jena-fuseki1-1.1.2-distribution.zip
  unzip jena-fuseki1-1.1.2-distribution.zip
  cd jena-fuseki1-1.1.2
- unzip -o /vagrant/dcat-ap_validator_v1.1.0.zip
+ unzip -o ${WORKDIR}/dcat-ap_validator_v1.1.0.zip
  npm install request
 popd
 
 ###################################################################################
 # Set up the DCAT validator to restart as a service
 
-pushd /vagrant
+pushd ${WORKDIR}
  cp dcat_service /etc/init.d
  cp start_dcat-ap_validator.sh /var/local/dcat-ap_validator/jena-fuseki1-1.1.2/pages
  cp dcat.js.config /var/local/dcat-ap_validator/jena-fuseki1-1.1.2/pages/js/concat/dcat.js
